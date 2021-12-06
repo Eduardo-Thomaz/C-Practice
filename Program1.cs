0ee7b14ac1e3b8f0b687fa45163830c2a7665a80
Eduardo Thomaz <93535399+Eduardo-Thomaz@users.noreply.github.com>
2021-12-06 12:52:44 -0300

    Analyzes array, adds 0 to negative entries.

---
+++ Program1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace C_Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+          double [] n1;
+          int arraySize = 10;
+
+          n1 = new double[arraySize];
+
+          Console.WriteLine("Inform the array: ");
+          for (int i = 0; i < arraySize; i++)
+          {
+
+            n1[i] = double.Parse(Console.ReadLine());
+            Console.WriteLine("Value is: {0:N}", n1[i]);
+
+          }
+
+          for (int i = 0; i < arraySize; i++)
+          {
+            if(n1[i] < 0)
+            {
+              n1[i] = 0;
+              Console.WriteLine("Value is: {0:N}", n1[i]);
+            }
+          }
+        }
+    }
+}
