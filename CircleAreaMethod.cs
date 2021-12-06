02f54443d530349f1f2147dda150a300f7ab8a9c
Eduardo Thomaz <93535399+Eduardo-Thomaz@users.noreply.github.com>
2021-12-06 16:12:18 -0300

    Method calculates area of circle.
    Pi = 3.14.

---
+++ CircleAreaMethod.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace C_Tests
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+          double radius;
+          double areaCircle;
+
+          Console.WriteLine("Inform the radius: ");
+          radius = double.Parse(Console.ReadLine());
+          areaCircle = CircleArea(radius);
+          Console.WriteLine("Area = {0:N}", areaCircle);
+
+        }
+        static double CircleArea(double radius)
+        {
+          double pi = 3.14;
+          double area = pi * radius * radius;
+          return area;
+        }
+
+    }
+} 
