9aebfd29fa4e98d1dfa8efb889a5869fd1b6f61e
Eduardo Thomaz <93535399+Eduardo-Thomaz@users.noreply.github.com>
2021-12-06 14:53:56 -0300

    Add files via upload

---
+++ Welcome.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace C_Tests
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+          Console.WriteLine("What's your name?");
+           string name = Console.ReadLine();
+           Console.WriteLine($"Hello {name}");
+           Welcome();
+        }
+        static void Welcome()
+        {
+            if(DateTime.Now.Hour >= 5 && DateTime.Now.Hour < 18)
+            {
+                Console.WriteLine("Good day!");
+            }
+            else
+            {
+                Console.WriteLine("Good night!");
+            }
+        }
+    }
+} 
