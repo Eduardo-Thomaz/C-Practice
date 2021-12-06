6e0244ad1772d8581a3d287713663b552b1ab3f2
Eduardo Thomaz <93535399+Eduardo-Thomaz@users.noreply.github.com>
2021-12-06 15:11:19 -0300

    Update Welcome.cs
    Added name parameter.

--- Welcome.cs
+++ Welcome.cs
@@ -9,17 +9,17 @@ namespace C_Tests
           Console.WriteLine("What's your name?");
            string name = Console.ReadLine();
            Console.WriteLine($"Hello {name}");
-           Welcome();
+           Welcome(name);
         }
-        static void Welcome()
+        static void Welcome(string user)
         {
             if(DateTime.Now.Hour >= 5 && DateTime.Now.Hour < 18)
             {
-                Console.WriteLine("Good day!");
+                Console.WriteLine($"Good day {user}!");
             }
             else
             {
-                Console.WriteLine("Good night!");
+                Console.WriteLine($"Good night {user}!");
             }
         }
     }
